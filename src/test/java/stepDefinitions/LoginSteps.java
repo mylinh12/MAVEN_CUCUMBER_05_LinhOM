6c85d4822674a52e46e0aaa3109b970bb6164d08
My Linh <shinichi121088@gmail.com>
2019-01-12 01:28:54 +0700

    Cucumber 02 - Topic 34 - Pass (Done 60%)

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -15,7 +15,7 @@ import cucumberOptions.Hooks;
 public class LoginSteps {
 
 	WebDriver driver;
-	static String userID, password, loginPageUrl, messagessName = "Welcome To Manager's Page of Guru99 Bank";
+	static String userID, password, loginPageUrl;
 
 	public LoginSteps() {
 		driver = Hooks.openBrowser();
@@ -59,83 +59,30 @@ public class LoginSteps {
 	@Then("^I get UserID infor$")
 	public void iGetUserIDInfor() {
 		userID = driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td")).getText();
-		System.out.println("userID = " + userID);
+		System.out.println("userID at Register page = " + userID);
 	}
 
 	@Then("^I get Password infor$")
 	public void iGetPasswordInfor() {
 		password = driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();
-		System.out.println("Password = " + password);
+		System.out.println("Password at Register page = " + password);
 	}
 
 	@Given("^I input to Username textbox$")
 	public void iInputToUsernameTextbox() {
+		System.out.println("userID at Login page = " + userID);
 		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(userID);
 	}
 
 	@When("^I input to Password textbox$")
 	public void iInputToPasswordTextbox() {
+		System.out.println("Password at Login page = " + password);
 		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
 	}
 
 	@When("^I click to Login button at Login page$")
 	public void iClickToLoginButtonAtLoginPage() {
 		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
-	}
-
-	@When("^I open New Customer page$")
-	public void iOpenNewCustomerPage() {
-
-	}
-
-	@When("^I input to New Customer form with data$")
-	public void iInputToNewCustomerFormWithData(DataTable arg1) {
-
-	}
-
-	@When("^I click to Submit button at New Customer page$")
-	public void iClickToSubmitButtonAtNewCustomerPage() {
-
-	}
-
-	@Then("^Verify message displayed with data \"([^\"]*)\"$")
-	public void verifyMessageDisplayedWithData(String arg1) {
-
-	}
-
-	@Then("^I verify all above information created success$")
-	public void iVerifyAllAboveInformationCreatedSuccess(DataTable arg1) {
-
-	}
-
-	@When("^I get Customer ID at New Customer page$")
-	public void iGetCustomerIDAtNewCustomerPage() {
-
-	}
-
-	@When("^I open Edit Customer page$")
-	public void iOpenEditCustomerPage() {
-
-	}
-
-	@When("^Input Customer ID to textbox$")
-	public void inputCustomerIDToTextbox() {
-
-	}
-
-	@When("^I click to Submit button at Edit Customer page$")
-	public void iClickToSubmitButtonAtEditCustomerPage() {
-
-	}
-
-	@When("^I input to Edit Customer form with data$")
-	public void iInputToEditCustomerFormWithData(DataTable arg1) {
-
-	}
-
-	@Then("^I verify all above information edited success$")
-	public void iVerifyAllAboveInformationEditedSuccess(DataTable arg1) {
-
 	}
 
 	public String randomEmail() {
